b7a1500bd1e855a0e05dda8285512eeb999ef186
Bruno Colpani <bruno.colpani1@gmail.com>
2024-01-19 19:05:09 -0300

    feat: Database configuration

---
+++ src/database.providers.ts
@@ -0,0 +1,23 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async (configService: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        database: configService.get<string>('DATABASE_NAME'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        entities: [__dirname + '/../**/*.entity.js'],
+        synchronize: false,
+      });
+
+      return dataSource.initialize();
+    },
+    inject: [ConfigService],
+  },
+];
